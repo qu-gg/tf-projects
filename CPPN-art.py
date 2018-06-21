ed676cc6e8def586d3eea5e0f2741dbb9bd71a2c
Ryan Missel <rxm7244@rit.edu>
2018-06-20 22:32:04 -0400

    Tensor shapes for initial hidden layer are off - Check how to input data

--- CPPN-art.py
+++ CPPN-art.py
@@ -3,10 +3,10 @@ import numpy as np
 from scipy import misc
 
 
-coords = tf.placeholder(tf.float32, (500, 500, 1, 1))
+coords = tf.placeholder(tf.float32, (None, 4))
 
 k, l, m, n = 200, 100, 60, 30
-w1 = tf.Variable(tf.truncated_normal([1, k], stddev=0.1))
+w1 = tf.Variable(tf.truncated_normal([4, k], stddev=0.1))
 b1 = tf.Variable(tf.zeros([k]))
 w2 = tf.Variable(tf.truncated_normal([k, l], stddev=0.1))
 b2 = tf.Variable(tf.zeros([l]))
@@ -25,16 +25,19 @@ pred = tf.nn.softmax(tf.matmul(y4, w5) + b5)
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
-    x, y = [], []
+    x, y = [0 for _ in range(500)], [0 for _  in range(500)]
     for n in range(500):
         x[n] = n
         y[n] = n
-    values = np.array(len(x) * len(y) * 3)
 
-    for m in range(500):
-        features = [x[m], y[m], 1, 50]
-        rgb = sess.run(pred, feed_dict={coords: features})
+    values = [[[]]]
 
+    for x_axis in range(500):
+        for y_axis in range(500):
+            features = [x[x_axis], y[y_axis], 1, 50]
+            rgb = sess.run(pred, feed_dict={coords: features})
+            values[x_axis][y_axis] = rgb
 
+    values = np.array(values)
     filename = input("Enter filename to save: ")
     misc.imsave("images/" + filename, values)
