56ace68892ed4ad8036c0013b1da34a026c98426
Ryan Missel <rxm7244@rit.edu>
2018-07-10 18:02:43 -0400

    Moving onto a different architecture for different testing

--- CPPN-3Layer.py
+++ CPPN-3Layer.py
@@ -10,25 +10,25 @@ y_size = 2800
 
 coords = tf.placeholder(tf.float32, (None, 4), name='features')
 
-k, l, m = 2500, 1500, 1500
+k, l, m = 5000, 5000, 5000
 
-w1 = tf.Variable(tf.random_normal([4, k], stddev=.5913, mean=0), name="Weights1")
-w2 = tf.Variable(tf.random_normal([k, l], stddev=.15499, mean=0), name="Weights2")
-w3 = tf.Variable(tf.random_normal([l, 3], stddev=.00184, mean=0), name="Weights3")
+w1 = tf.Variable(tf.random_normal([4, k], stddev=.5713**2, mean=0), name="Weights1")
+w2 = tf.Variable(tf.random_normal([k, l], stddev=.35499**2, mean=0), name="Weights2")
+w3 = tf.Variable(tf.random_normal([l, 3], stddev=.00184**2, mean=0), name="Weights3")
 
-y1 = tf.nn.tanh(tf.matmul(coords, w1), name="HLayer1")
-y2 = tf.nn.tanh(tf.matmul(y1, w2), name="HLayer2")
-pred = tf.nn.tanh(tf.matmul(y2, w3), name="OutputLayer")
+y1 = tf.nn.relu6(tf.matmul(coords, w1), name="HLayer1")
+y2 = tf.nn.relu6(tf.matmul(y1, w2), name="HLayer2")
+pred = tf.nn.relu6(tf.matmul(y2, w3), name="OutputLayer")
 
 
 def create_array():
     parameters = [[[] for _ in range(x_size)] for _ in range(y_size)]
     for y in range(y_size):
         for x in range(x_size):
-            added_x = x + 150
-            added_y = y + 150
+            added_x = x + 50
+            added_y = y + 50
             radius = np.sqrt(added_x ** 2 + added_y ** 2)
-            parameters[y][x] = [added_x, added_y, radius, rand]
+            parameters[y][x] = [added_y, added_x, radius, rand]
     return parameters
 
 
@@ -36,7 +36,7 @@ config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
 with tf.Session(config=config) as sess:
     sess.run(tf.global_variables_initializer())
-    rand = 1800
+    rand = 1500
 
     features = create_array()
 
