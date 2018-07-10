ff208b696cafc3045de34c4f4d73a3d10630fc57
Ryan Missel <rxm7244@rit.edu>
2018-07-09 21:54:49 -0400

    Separated x and y sizes for easier img dimension setting

--- CPPN-3Layer.py
+++ CPPN-3Layer.py
@@ -5,15 +5,16 @@ tf.logging.set_verbosity(tf.logging.ERROR)
 import numpy as np
 from scipy import misc
 
-img_size = 1400
+x_size = 1400
+y_size = 2800
+
 coords = tf.placeholder(tf.float32, (None, 4), name='features')
 
-k, l, m = 5000, 5000, 5000
-B = 250
+k, l, m = 2500, 1500, 1500
 
 w1 = tf.Variable(tf.random_normal([4, k], stddev=.5913, mean=0), name="Weights1")
-w2 = tf.Variable(tf.random_normal([k, l], stddev=.35499, mean=0), name="Weights2")
-w3 = tf.Variable(tf.random_normal([l, 3], stddev=.2184, mean=0), name="Weights3")
+w2 = tf.Variable(tf.random_normal([k, l], stddev=.15499, mean=0), name="Weights2")
+w3 = tf.Variable(tf.random_normal([l, 3], stddev=.00184, mean=0), name="Weights3")
 
 y1 = tf.nn.tanh(tf.matmul(coords, w1), name="HLayer1")
 y2 = tf.nn.tanh(tf.matmul(y1, w2), name="HLayer2")
@@ -21,16 +22,13 @@ pred = tf.nn.tanh(tf.matmul(y2, w3), name="OutputLayer")
 
 
 def create_array():
-    parameters = [[[] for _ in range(img_size)] for _ in range(img_size)]
-    neg_x = -(img_size / 2)
-    neg_y = -(img_size / 2)
-    for x in range(img_size):
-        for y in range(img_size):
-            radius = np.sqrt(neg_x ** 2 + neg_y ** 2)
-            parameters[x][y] = [x, y, radius, rand]
-            neg_y += 1
-        neg_x += 1
-        neg_y = 0
+    parameters = [[[] for _ in range(x_size)] for _ in range(y_size)]
+    for y in range(y_size):
+        for x in range(x_size):
+            added_x = x + 150
+            added_y = y + 150
+            radius = np.sqrt(added_x ** 2 + added_y ** 2)
+            parameters[y][x] = [added_x, added_y, radius, rand]
     return parameters
 
 
@@ -38,15 +36,15 @@ config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
 with tf.Session(config=config) as sess:
     sess.run(tf.global_variables_initializer())
-    rand = 750
+    rand = 1800
 
     features = create_array()
 
-    rgb = [0 for _ in range(img_size)]
-    for batch in range(img_size):
+    rgb = [0 for _ in range(y_size)]
+    for batch in range(y_size):
         feed = features[batch]
         result = sess.run(pred, feed_dict={coords: feed})
         rgb[batch] = result
 
-msave("images/" + "testing.png", rgb)
+    misc.imsave("images/" + "testing.png", rgb)
 
