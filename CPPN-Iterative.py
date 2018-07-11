51c658013fa8e824f6a269e369127dcc08ebf807
Ryan Missel <rxm7244@rit.edu>
2018-07-10 20:21:33 -0400

    Renamed CPPN-3D to CPPN-Iterative because of the changed direction of the file
    Trying to see if iterative generation of weights and layers is possible in TensorFlow

---
+++ CPPN-Iterative.py
@@ -0,0 +1,68 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import tensorflow as tf
+tf.logging.set_verbosity(tf.logging.ERROR)
+import numpy as np
+from scipy import misc
+import random
+
+x_size = 250
+y_size = 250
+
+coords = tf.placeholder(tf.float32, (None, 4), name='features')
+
+
+def iterative_weights(num_neurons, initial, layers):
+    weight_list = []
+    for step in range(layers):
+        if step == 0:
+            new_weight = tf.Variable(tf.truncated_normal([initial, num_neurons], stddev=.5500))
+            weight_list.append(new_weight)
+        if step != 0 and step < layers:
+            new_weight = tf.Variable(tf.truncated_normal([num_neurons, num_neurons], stddev=.2500))
+            weight_list.append(new_weight)
+        if step == layers:
+            new_weight = tf.Variable(tf.truncated_normal([num_neurons, 3], stddev=.0145))
+            weight_list.append(new_weight)
+    return weight_list
+
+
+def iterative_layers(weight_list, prev_output, layers):
+    calculation = 0
+    test = prev_output
+    for step in range(layers):
+        calculation = tf.nn.tanh(tf.matmul(test, weight_list[step]))
+        test = calculation
+    return calculation
+
+
+weights = iterative_weights(1500, 4, 3)
+pred = iterative_layers(weights, coords, 3)
+
+
+def create_array():
+    parameters = [[[] for _ in range(x_size)] for _ in range(y_size)]
+    for y in range(y_size):
+        for x in range(x_size):
+            added_x = x + random.randint(25,50)
+            added_y = y + random.randint(25,50)
+            radius = np.sqrt(added_x ** 2 + added_y ** 2)
+            parameters[y][x] = [added_y, added_x, radius, rand]
+    return parameters
+
+
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+with tf.Session(config=config) as sess:
+    sess.run(tf.global_variables_initializer())
+    rand = 1500
+
+    features = create_array()
+
+    rgb = [0 for _ in range(y_size)]
+    for batch in range(y_size):
+        feed = features[batch]
+        result = sess.run(pred, feed_dict={coords: feed})
+        rgb[batch] = result
+
+    misc.imsave("images/" + "testing.png", rgb)
