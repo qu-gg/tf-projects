2f22b7869f3f7459dd54033d3242950d180e8dd6
Ryan Missel <rxm7244@rit.edu>
2018-07-17 17:40:23 -0400

    Reverted to earlier, cleaner code. Added num_layers variable for function parameters

--- CPPN-Iterative.py
+++ CPPN-Iterative.py
@@ -5,24 +5,24 @@ tf.logging.set_verbosity(tf.logging.ERROR)
 import numpy as np
 from scipy import misc
 
-x_size = 1400
-y_size = 2800
+x_size = 1920
+y_size = 1080
 
-coords = tf.placeholder(tf.float32, (None, 2), name='features')
+coords = tf.placeholder(tf.float32, (None, 4), name='features')
 
 
 def iterative_weights(num_neurons, initial, layers):
     weight_list = []
-    initial_weight = tf.Variable(tf.truncated_normal([initial, num_neurons], stddev=.099812300))
+    initial_weight = tf.Variable(tf.truncated_normal([initial, num_neurons], stddev=.5500))
     weight_list.append(initial_weight)
 
     counter = 1
     while counter < layers - 1:
-        new_weight = tf.Variable(tf.truncated_normal([num_neurons, num_neurons], stddev=.0144500))
+        new_weight = tf.Variable(tf.truncated_normal([num_neurons, num_neurons], stddev=.2500))
         weight_list.append(new_weight)
         counter += 1
 
-    final_weight = tf.Variable(tf.truncated_normal([num_neurons, 3], stddev=.02145))
+    final_weight = tf.Variable(tf.truncated_normal([num_neurons, 3], stddev=.0145))
     weight_list.append(final_weight)
     return weight_list
 
@@ -36,8 +36,8 @@ def iterative_layers(weight_list, prev_output, layers):
     return calculation
 
 
-num_layers = 9
-weights = iterative_weights(3000, 2, num_layers)
+num_layers = 5
+weights = iterative_weights(1500, 4, num_layers)
 pred = iterative_layers(weights, coords, num_layers)
 
 
@@ -45,10 +45,10 @@ def create_array():
     parameters = [[[] for _ in range(x_size)] for _ in range(y_size)]
     for y in range(y_size):
         for x in range(x_size):
-            added_x = x + 0
-            added_y = y + 0
-            # radius = np.sqrt(added_x ** 2 + added_y ** 2)
-            parameters[y][x] = [added_x, added_y]
+            added_x = x + 50
+            added_y = y + 50
+            radius = np.sqrt(added_x ** 2 + added_y ** 2)
+            parameters[y][x] = [added_y, added_x, radius, rand]
     return parameters
 
 
@@ -56,11 +56,14 @@ config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
 with tf.Session(config=config) as sess:
     sess.run(tf.global_variables_initializer())
+    rand = 1500
+
     features = create_array()
+
     rgb = [0 for _ in range(y_size)]
     for batch in range(y_size):
         feed = features[batch]
         result = sess.run(pred, feed_dict={coords: feed})
         rgb[batch] = result
 
-    misc.imsave("images/testing.png", rgb)
+    misc.imsave("images/" + "testing.png", rgb)
