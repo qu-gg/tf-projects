be0ef7664606246613bcf80a692983c18d688646
Ryan Missel <rxm7244@rit.edu>
2018-07-28 15:08:15 -0400

    Simplified code snippits, added a regularization at the end of each calculation.

--- CPPN/CPPN-Iterative.py
+++ CPPN/CPPN-Iterative.py
@@ -13,14 +13,14 @@ coords = tf.placeholder(tf.float32, (None, 4), name='features')
 
 def iterative_weights(num_neurons, initial, layers):
     weight_list = []
-    initial_weight = tf.Variable(tf.truncated_normal([initial, num_neurons], stddev=.5500))
+    initial_weight = tf.Variable(tf.truncated_normal([initial, num_neurons], stddev=.55))
     weight_list.append(initial_weight)
 
     for _ in range(layers - 2):
-        new_weight = tf.Variable(tf.truncated_normal([num_neurons, num_neurons], stddev=.2500))
+        new_weight = tf.Variable(tf.truncated_normal([num_neurons, num_neurons], stddev=.220))
         weight_list.append(new_weight)
 
-    final_weight = tf.Variable(tf.truncated_normal([num_neurons, 3], stddev=.0145))
+    final_weight = tf.Variable(tf.random_normal([num_neurons, 3]))
     weight_list.append(final_weight)
     return weight_list
 
@@ -33,7 +33,7 @@ def iterative_layers(weight_list, prev_output, layers):
 
 
 num_layers = 6
-weights = iterative_weights(2000, 4, num_layers)
+weights = iterative_weights(3500, 4, num_layers)
 pred = iterative_layers(weights, coords, num_layers)
 
 
@@ -41,10 +41,12 @@ def create_array():
     parameters = [[[] for _ in range(x_size)] for _ in range(y_size)]
     for y in range(y_size):
         for x in range(x_size):
-            added_x = x + 50
-            added_y = y + 50
+            added_x = abs(x - x_size / 2)
+            added_y = abs(y - y_size / 2)
+
             radius = np.sqrt(added_x ** 2 + added_y ** 2)
-            parameters[y][x] = [added_y, added_x, radius, rand]
+            radius = np.cos(10 * radius)
+            parameters[y][x] = [x, y, radius, rand]
     return parameters
 
 
@@ -52,7 +54,7 @@ config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
 with tf.Session(config=config) as sess:
     sess.run(tf.global_variables_initializer())
-    rand = 1750
+    rand = 2000
 
     features = create_array()
 
