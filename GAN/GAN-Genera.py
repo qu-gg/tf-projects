5949d1743ec085b21480c3ec159c7be23669aaee
Ryan Missel <rxm7244@rit.edu>
2018-07-28 20:08:35 -0400

    Ending up with 28x28x1 with input noise changes

--- GAN/GAN-Genera.py
+++ GAN/GAN-Genera.py
@@ -5,17 +5,23 @@ tf.logging.set_verbosity(tf.logging.ERROR)
 import numpy as np
 from scipy import misc
 
-ary = np.random.uniform(0, 1, 100)
-img_input = tf.cast(tf.reshape(ary, [-1, 10, 10, 1]), tf.float32)
+ary = np.random.uniform(0, 1, 49)
+img_input = tf.cast(tf.reshape(ary, [-1, 7, 7, 1]), tf.float32)
 
 net = tf.layers.conv2d_transpose(inputs=img_input, filters=1024, kernel_size=0, padding='same')
 
 net = tf.layers.conv2d_transpose(inputs=net, filters=512, kernel_size=5, padding='same')
 
-net = tf.layers.conv2d_transpose(inputs=net, filters=256, kernel_size=5, padding='same', strides=2)
+net = tf.layers.conv2d_transpose(inputs=net, filters=256, kernel_size=5, padding='same')
 
 net = tf.layers.conv2d_transpose(inputs=net, filters=128, kernel_size=5, padding='same', strides=2)
 
 net = tf.layers.conv2d_transpose(inputs=net, filters=1, kernel_size=5, padding='same', strides=2)
 
+print(net)
+
+net = tf.layers.flatten(net)
+print(net)
+
+net = tf.reshape(net, [28, 28, 1])
 print(net)
