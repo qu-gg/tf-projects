8963b22662f40b68aa4fe93fc800b1e5948dcbbb
Ryan Missel <rxm7244@rit.edu>
2018-07-28 20:03:29 -0400

    Basis for deconv network, figuring out how to end up with 28x28x1 images

--- GAN/GAN-Genera.py
+++ GAN/GAN-Genera.py
@@ -2,4 +2,20 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import tensorflow as tf
 tf.logging.set_verbosity(tf.logging.ERROR)
+import numpy as np
+from scipy import misc
 
+ary = np.random.uniform(0, 1, 100)
+img_input = tf.cast(tf.reshape(ary, [-1, 10, 10, 1]), tf.float32)
+
+net = tf.layers.conv2d_transpose(inputs=img_input, filters=1024, kernel_size=0, padding='same')
+
+net = tf.layers.conv2d_transpose(inputs=net, filters=512, kernel_size=5, padding='same')
+
+net = tf.layers.conv2d_transpose(inputs=net, filters=256, kernel_size=5, padding='same', strides=2)
+
+net = tf.layers.conv2d_transpose(inputs=net, filters=128, kernel_size=5, padding='same', strides=2)
+
+net = tf.layers.conv2d_transpose(inputs=net, filters=1, kernel_size=5, padding='same', strides=2)
+
+print(net)
