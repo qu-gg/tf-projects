45f4b9498012e68da371c4440304766a679c4366
Ryan Missel <rxm7244@rit.edu>
2018-07-29 09:56:21 -0400

    Renamed layeres, added TF sess Getting weird Cuda version check error - perhaps with layers API?

--- GAN/GAN-Genera.py
+++ GAN/GAN-Genera.py
@@ -1,27 +1,29 @@
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import tensorflow as tf
-tf.logging.set_verbosity(tf.logging.ERROR)
 import numpy as np
 from scipy import misc
 
 ary = np.random.uniform(0, 1, 49)
 img_input = tf.cast(tf.reshape(ary, [-1, 7, 7, 1]), tf.float32)
 
-net = tf.layers.conv2d_transpose(inputs=img_input, filters=1024, kernel_size=0, padding='same')
+conv_one = tf.layers.conv2d_transpose(inputs=img_input, filters=1024, kernel_size=0, padding='same')
 
-net = tf.layers.conv2d_transpose(inputs=net, filters=512, kernel_size=5, padding='same')
+conv_two = tf.layers.conv2d_transpose(inputs=conv_one, filters=512, kernel_size=5, padding='same')
 
-net = tf.layers.conv2d_transpose(inputs=net, filters=256, kernel_size=5, padding='same')
+conv_three = tf.layers.conv2d_transpose(inputs=conv_two, filters=256, kernel_size=5, padding='same')
 
-net = tf.layers.conv2d_transpose(inputs=net, filters=128, kernel_size=5, padding='same', strides=2)
+conv_four = tf.layers.conv2d_transpose(inputs=conv_three, filters=128, kernel_size=5, padding='same', strides=2)
 
-net = tf.layers.conv2d_transpose(inputs=net, filters=1, kernel_size=5, padding='same', strides=2)
+final_layer = tf.layers.conv2d_transpose(inputs=conv_four, filters=1, kernel_size=5, padding='same', strides=2)
 
-print(net)
+flattened = tf.layers.flatten(final_layer)
 
-net = tf.layers.flatten(net)
-print(net)
+output = tf.reshape(flattened, [28, 28])
 
-net = tf.reshape(net, [28, 28, 1])
-print(net)
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+
+    fake = sess.run(output)
+    misc.toimage(fake).show()
+
+sess.close()
