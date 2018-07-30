8dc1f4ddc366479e088090fbba3adab85ca13b82
Ryan Missel <rxm7244@rit.edu>
2018-07-30 15:26:49 -0400

    Added create_batch function to create training batches, added weight initializers to conv layers

--- GAN/Discriminator.py
+++ GAN/Discriminator.py
@@ -8,18 +8,24 @@ x_reshape = tf.reshape(input_x, [-1, 28, 28, 1])
 
 input_cls = tf.placeholder(tf.float32, [None, 1])
 
-conv1 = tf.layers.conv2d(inputs=x_reshape, padding='same', filters=1, kernel_size=5, strides=2, activation=tf.nn.relu)
+conv1_w = tf.Variable(tf.truncated_normal([28, 28, 1, 3]))
+conv1_b = tf.zeros([1])
+conv1 = tf.nn.conv2d(x_reshape, conv1_w, padding='SAME', strides=[1, 1, 1, 1]) + conv1_b
 
-conv2 = tf.layers.conv2d(inputs=conv1, padding='same', filters=128, kernel_size=5, strides=2, activation=tf.nn.relu)
+conv2 = tf.layers.conv2d(inputs=conv1, padding='same', filters=128, kernel_size=5, strides=2, activation=tf.nn.relu,
+                         kernel_initializer=tf.truncated_normal_initializer(stddev=.001))
 
-conv3 = tf.layers.conv2d(inputs=conv2, padding='same', filters=256, kernel_size=5, activation=tf.nn.relu)
+conv3 = tf.layers.conv2d(inputs=conv2, padding='same', filters=256, kernel_size=5, strides=2, activation=tf.nn.relu,
+                         kernel_initializer=tf.truncated_normal_initializer(stddev=.001))
 
-conv4 = tf.layers.conv2d(inputs=conv3, padding='same', filters=512, kernel_size=5, activation=tf.nn.relu)
+conv4 = tf.layers.conv2d(inputs=conv3, padding='same', filters=512, kernel_size=5, strides=2, activation=tf.nn.relu,
+                         kernel_initializer=tf.truncated_normal_initializer(stddev=.001))
 
 flatten = tf.layers.flatten(conv4)
 
-# Fully connected layer
-pred = tf.layers.dense(inputs=flatten, units=1, activation=tf.nn.softmax)
+pred = tf.layers.dense(inputs=flatten, units=1, activation=tf.nn.relu,
+                       kernel_initializer=tf.truncated_normal_initializer(stddev=.001))
+
 
 # Loss, cost, optimizing
 loss = tf.nn.softmax_cross_entropy_with_logits_v2(labels=input_cls, logits=pred)
@@ -31,35 +37,57 @@ correct_prediction = tf.equal(input_cls, pred)
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 
-# train function
+def create_batch(mnist, batch_size):
+    '''
+    Function for creating a single training batch for the discriminator, mixed of real and fake images
+    Class is 1 if it is real and 0 if it is fake
+    :param mnist:
+    :param mnist_cls:
+    :param batch_size:
+    :return:
+    '''
+    generated = generate_img(batch_size)
+
+    image_batch = []
+    class_batch = []
+
+    even_index = 0
+    odd_index = 0
+    for i in range(batch_size * 2):
+        if i % 2 == 0:
+            image_batch.append(mnist[even_index])
+            class_batch.append(1)
+            even_index += 1
+        else:
+            image_batch.append(generated[odd_index])
+            class_batch.append(0)
+            odd_index += 1
+    return image_batch, class_batch
+
+
 def train_discrim(num_iter):
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
     sess = tf.Session(config=config)
+    sess.run(tf.global_variables_initializer())
 
-    with tf.device("/gpu:0"):
-        sess.run(tf.global_variables_initializer())
+    train_batch_size = 64
 
-        train_batch_size = 64
+    for i in range(num_iter):
+        x_batch = data.train.next_batch(train_batch_size)
+        print(x_batch)
+        image_batch, class_batch = create_batch(x_batch, train_batch_size)
 
-        for i in range(num_iter):
-            x_batch, y_true_batch = data.train.next_batch(train_batch_size)
+        feed_dict_train = {input_x: image_batch,
+                           input_cls: class_batch}
 
-            feed_dict_train = {input_x: x_batch,
-                               input_cls: 0}
+        sess.run(optimizer, feed_dict=feed_dict_train)
 
-            sess.run(optimizer, feed_dict=feed_dict_train)
+        # accuracy
+        print("Acc on ", i, ": ", sess.run(accuracy, feed_dict=feed_dict_train))
+
 
     sess.close()
 
 
-config = tf.ConfigProto()
-config.gpu_options.allow_growth = True
-with tf.Session(config=config) as sess:
-    sess.run(tf.global_variables_initializer())
-
-    image = generate_img()
-    result = sess.run(pred, feed_dict={input_x: image})
-    print(result)
-
-sess.close()
+train_discrim(10)
