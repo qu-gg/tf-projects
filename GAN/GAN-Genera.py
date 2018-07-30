290bf7d1e0382cdf7f93b67b6505456730281b92
Ryan Missel <rxm7244@rit.edu>
2018-07-30 11:02:27 -0400

    Changed way of input, as the way to use the conv2d_transpose is to compute the gradient of the regular conv operation for use in the tranpose.
    Kernel size of 0 way causing issues with layer outputs

--- GAN/GAN-Genera.py
+++ GAN/GAN-Genera.py
@@ -1,29 +1,47 @@
-import os
 import tensorflow as tf
 import numpy as np
 from scipy import misc
 
-ary = np.random.uniform(0, 1, 49)
-img_input = tf.cast(tf.reshape(ary, [-1, 7, 7, 1]), tf.float32)
+x_input = tf.placeholder(tf.float32, [None, 49])
+img_input = tf.cast(tf.reshape(x_input, [-1, 7, 7, 1]), tf.float32)
 
-conv_one = tf.layers.conv2d_transpose(inputs=img_input, filters=1024, kernel_size=0, padding='same')
+weights_init = tf.truncated_normal_initializer(stddev=.05)
 
-conv_two = tf.layers.conv2d_transpose(inputs=conv_one, filters=512, kernel_size=5, padding='same')
+conv1_w = tf.Variable(tf.truncated_normal([7, 7, 1, 32]))
+conv1_b = tf.zeros([32])
+conv1 = tf.nn.conv2d(img_input, conv1_w, strides=[1, 1, 1, 1], padding='SAME') + conv1_b
 
-conv_three = tf.layers.conv2d_transpose(inputs=conv_two, filters=256, kernel_size=5, padding='same')
+conv_one = tf.layers.conv2d_transpose(inputs=conv1, filters=1024, kernel_size=5, padding='same',
+                                      activation=tf.nn.relu)
 
-conv_four = tf.layers.conv2d_transpose(inputs=conv_three, filters=128, kernel_size=5, padding='same', strides=2)
+conv_two = tf.layers.conv2d_transpose(inputs=conv_one, filters=512, kernel_size=5, padding='same',
+                                      activation=tf.nn.relu, kernel_initializer=weights_init)
 
-final_layer = tf.layers.conv2d_transpose(inputs=conv_four, filters=1, kernel_size=5, padding='same', strides=2)
+conv_three = tf.layers.conv2d_transpose(inputs=conv_two, filters=256, kernel_size=5, padding='same',
+                                        activation=tf.nn.relu, kernel_initializer=weights_init)
+
+conv_four = tf.layers.conv2d_transpose(inputs=conv_three, filters=128, kernel_size=5, padding='same', strides=2,
+                                       activation=tf.nn.relu, kernel_initializer=weights_init)
+
+final_layer = tf.layers.conv2d_transpose(inputs=conv_four, filters=1, kernel_size=5, padding='same', strides=2,
+                                         activation=tf.nn.relu, kernel_initializer=weights_init)
 
 flattened = tf.layers.flatten(final_layer)
 
-output = tf.reshape(flattened, [28, 28])
+output = tf.reshape(flattened, [-1, 28])
 
-with tf.Session() as sess:
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+with tf.Session(config=config) as sess:
     sess.run(tf.global_variables_initializer())
 
+    reshaped = sess.run(img_input, feed_dict={x_input: [np.random.uniform(0, 1, 49)]})
+
+    conv = sess.run(conv_one, feed_dict={x_input: [np.random.uniform(0,1, 49)]})
+    print(conv)
+
     fake = sess.run(output)
-    misc.toimage(fake).show()
+    print(fake)
+    # misc.toimage(fake).show()
 
 sess.close()
