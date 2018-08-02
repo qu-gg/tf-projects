d0ee37327bd687a719682251f106fdb87d5cbb2d
Ryan Missel <rxm7244@rit.edu>
2018-08-02 11:22:01 -0400

    Cleaned up train_gen to only run the optimizer
    Generate_img now returns a matrix of images in one session
    
    Main is moved, mostly unused

--- GAN/Generator.py
+++ GAN/Generator.py
@@ -1,13 +1,12 @@
 import tensorflow as tf
 import numpy as np
 from scipy import misc
-from GAN.Discriminator import use_discrim
 
 x_input = tf.placeholder(tf.float32, [None, 49])
 img_input = tf.cast(tf.reshape(x_input, [-1, 7, 7, 1]), tf.float32)
 
 conv1_w = tf.Variable(tf.truncated_normal([7, 7, 1, 32]))
-conv1_b = tf.zeros([32])
+conv1_b = tf.Variable(tf.zeros([32]))
 conv1 = tf.nn.conv2d(img_input, conv1_w, strides=[1, 1, 1, 1], padding='SAME') + conv1_b
 
 conv_one = tf.layers.conv2d_transpose(inputs=conv1, filters=1024, kernel_size=5, padding='same',
@@ -33,46 +32,41 @@ cost = tf.reduce_mean(loss)
 optimizer = tf.train.AdamOptimizer(learning_rate=.00001).minimize(cost)
 
 
-def train_gen():
+def train_gen(num, entropy):
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
     with tf.Session(config=config) as sess:
         sess.run(tf.global_variables_initializer())
 
-        # add loss calculations here
-        for _ in range(5):
-            entropy = use_discrim()
+        for _ in range(num):
             sess.run(optimizer, feed_dict={loss: entropy})
 
-        image = sess.run(output, feed_dict={x_input: []})
-        misc.toimage(image).show()
 
-
-def generate_img(num_runs):
+def generate_img(num_runs=1):
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
     with tf.Session(config=config) as sess:
         sess.run(tf.global_variables_initializer())
-        fakes = []
-        for _ in range(num_runs):
-            fake = sess.run(output, feed_dict={x_input: [np.random.uniform(0, 1, 49)]})
-            fakes.append(fake[0])
+        fakes = [[np.random.uniform(0, 1, 49)] for _ in range(num_runs)]
+        fake = sess.run(output, feed_dict={x_input: fakes})
+        print(fake)
+        fakes.append(fake)
 
     sess.close()
     return fakes
 
 
+def main():
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    with tf.Session(config=config) as sess:
+        sess.run(tf.global_variables_initializer())
+
+        train_gen()
+
+    sess.close()
+
+
 if __name__ == "__main__":
-    def main():
-        config = tf.ConfigProto()
-        config.gpu_options.allow_growth = True
-        with tf.Session(config=config) as sess:
-            sess.run(tf.global_variables_initializer())
+    generate_img(10)
 
-            fake = sess.run(reshaped, feed_dict={x_input: [np.random.uniform(0, 1, 49)]})
-            misc.toimage(fake).show()
-
-
-        sess.close()
-
-    main()
